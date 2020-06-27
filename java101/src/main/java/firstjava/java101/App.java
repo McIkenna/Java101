33a39519c5213ea470b75beeadd7d61a99dbbe64
sontoly <sontoly@gmail.com>
2020-06-26 20:10:35 -0400

    Assignment 3

--- java101/src/main/java/firstjava/java101/App.java
+++ java101/src/main/java/firstjava/java101/App.java
@@ -10,24 +10,22 @@ public class App
     public static void main( String[] args )
     {
     	
-    	int totalPrice;
-    	int price = 3;
+    	
+    	/* 
+    	 int totalPrice;
+    	int price = 6;
     	int quantity = 8;
     	Double modulus = 0.05;
     	totalPrice = quantity*price;
-    	/* 
     	//Changes
+    
     	System.out.println( "total Price before discount: " + totalPrice );
     	if(totalPrice > 20) {
     		int newTotalPrice;
     		newTotalPrice = totalPrice - (2*price);
     		System.out.println( "total Price after discount: " + newTotalPrice );
     	}
-    	if(price > 5) {
-    		Double newPrice;
-    		newPrice = price + (totalPrice * modulus);
-    		System.out.println( "New price after discount: " + newPrice );
-    	}
+    	
     	
     	String quantityBox = "3";
     	String pricePerItemBox = "8";
@@ -71,18 +69,72 @@ public class App
     	
     	int xy = 2*x + (y + 3*x * 4*y) - 2*x;
     	System.out.println("Answer is : " + xy);
-    		*/
+    		
+    	//ASSIGNMENT 1
+    	if(price > 5) {
+    		Double newPrice;
+    		newPrice = price + (totalPrice * modulus);
+    		System.out.println( "New price after discount: " + newPrice );
+    	}
+    	
+    	//ASSIGNMENT 2
+    	
+    	int totalPrice;
+    	int price = 30;
+    	
+    	Scanner quant = new Scanner(System.in);
+    	System.out.print("Enter the Quantity you want: ");
+    	int quantity = quant.nextInt();
+    	Double disPercentage = 0.1;
+    	totalPrice = quantity*price;
     	
     	Scanner promo = new Scanner(System.in);
     	System.out.print(" Enter Promo Code : ");
     	String promoCode = promo.next().toUpperCase();
     	String code = "SUMMER2020";
-    	
     	System.out.println("promo code is : " + promoCode);
-    	if(promoCode.equals(code) ) {
+    	
+    	if(promoCode.equals(code) && totalPrice > 200) {
     		
-    		Double newPromoTotal = totalPrice - totalPrice*0.1;
+    		Double newPromoTotal = totalPrice * (1 - disPercentage);
     		 System.out.print("Disounted Promo Price: " + newPromoTotal);
     	}
+    	else {
+    		System.out.print(" The total Price is : " + totalPrice);
+    		} 
+    		
+    		*/
+    	//ASSIGNMENT 3
+    	Scanner grade = new Scanner(System.in);
+    	System.out.print("Enter your grade: ");
+    	Double grades = grade.nextDouble();
+    	
+    	if(grades >= 0 && grades <= 39) {
+    		System.out.println("You got an F, Failed");
+    		
+    	}
+    	
+    	else if(grades >= 40 && grades <= 49) {
+    		System.out.println("You Got an E: Passed");
+    	}
+    	
+    	else if(grades >= 50 && grades <= 59) {
+    		System.out.println("You Got a D: Passed");
+    	}
+    	
+    	else if(grades >= 60 && grades <= 69) {
+    		System.out.println("You Got a C: Passed");
+    	}
+    	else if(grades >= 70 && grades <= 85) {
+    		System.out.println("You Got a B: Good!");
+    	}
+    	
+    	else if(grades >= 86 && grades <= 100) {
+    		System.out.println("You Got an A: Excellent");
+    	}
+    	else {
+    			System.out.print("Not a valid Grade");
+    			};
+    	
     }
 }
